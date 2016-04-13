c99fc0a727c89705439513cfe0a5220ded454ecf
Rubén López Gómez <loslopez1@icloud.com>
2016-04-13 15:22:57 +0200

    add: require bump task before publish

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         'npm-publish': {
             options: {
+                requires: ['bump'],
                 abortIfDirty: true,
                 tag: 'latest'
             }
