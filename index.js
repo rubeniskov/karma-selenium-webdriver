3753fe42768542b1c3c81fc893f822cb44af5ba7
Rubén López Gómez <loslopez1@icloud.com>
2016-04-14 01:09:13 +0200

    fix: watch timeout session

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const wd = require('wd'),
         return res;
     };
 
-var SeleniumWebDriver = function(baseBrowserDecorator, cfg, args, id, logger) {
+var SeleniumWebDriver = function(baseBrowserDecorator, cfg, args, id, injector, emitter, logger) {
 
     baseBrowserDecorator(this);
 
     var self = this,
-        log = logger.create('SeleniumWebDriver'),
+        log = logger.create('selenium-webdriver'),
         config = configure({
             protocol: cfg.protocol || 'http',
             hostname: cfg.hostname,
@@ -36,27 +36,20 @@ var SeleniumWebDriver = function(baseBrowserDecorator, cfg, args, id, logger) {
             version: ''
         }, args),
         scope = {
-            sessionIsReady: false,
-            pendingHeartBeat: true,
             promises: {},
-            heartbeat: function(driver) {
-                scope.pendingHeartBeat = setTimeout(function() {
-                    log.debug('Heartbeat to Selenium Hub (%s) - fetching session', spec.browserName);
-                    driver.title(function(err, title) {
-                        if (err || title !== 'Karma') {
-                            log.error('Heartbeat to %s failed %s', spec.browserName, err ? scope.formatError(err) : 'Service unavalible');
-                            clearTimeout(scope.pendingHeartBeat);
-                            return self._done();
-                        }
-                        log.debug('Heartbeat to Selenium Hub (%s) - session active', spec.browserName);
-                        scope.heartbeat(driver);
-                    });
-                }, 60000)
-            },
+            intervals: {},
             formatError(err) {
                 return err.message + '\n' + (err.data ? '  ' + err.data : '')
             }
-        };
+        },
+        ourl = (function(surl) {
+            return configure(surl, {
+                host: surl.hostname + ':' + surl.port,
+                query: {
+                    id: id
+                }
+            });
+        })(configure(browser));
 
     if (!spec.browserName) {
         throw new Error('browserName is required!');
@@ -67,30 +60,55 @@ var SeleniumWebDriver = function(baseBrowserDecorator, cfg, args, id, logger) {
     self.name = spec.browserName + ' via Remote SeleniumWebDriver';
     self.id = id;
 
-    self.start = function(kurl) {
-        kurl = configure(url.parse(kurl), browser);
-        kurl = url.format(configure(kurl, {
-            host: kurl.hostname + ':' + kurl.port,
-            query: {
-                id: id
-            }
-        }));
+    self._start = function() {
+        var kurl = url.format(ourl);
 
         self.browser = wd.remote(config);
         self.browser.init(spec, function(err) {
             if (err)
                 return log.error('Browser %s driver initialize failed %s', spec.browserName, scope.formatError(err));
             log.debug('Browser %s driver connecting %s', spec.browserName, kurl);
-            self.browser.get(kurl, function(err) {
-                if (err)
-                    return log.error('Browser driver failed %s %s', kurl, scope.formatError(err));
-                log.debug('Browser %s driver connected %s', spec.browserName, kurl);
-                scope.heartbeat(self.browser);
-            });
+
+            if (scope.session) {
+                self.browser.attach(scope.session, function(err, capabilities) {
+                    if (err) {
+                        log.debug('Browser driver failed to attach session %s %s', scope.session, scope.formatError(err));
+                        log.info('Trying create new session');
+                        scope.session = null;
+                        self._start();
+                    }
+                    self.markCaptured();
+                    log.info('Browser driver session attached %s', scope.session);
+                });
+            } else {
+                self.browser.get(kurl, function(err) {
+                    if (err)
+                        return log.error('Browser driver failed %s %s', kurl, scope.formatError(err));
+                    log.debug('Browser %s driver connected %s', spec.browserName, kurl);
+
+                    self.getSession(function(sessionID) {
+                        scope.session = sessionID;
+                        self.markCaptured();
+                        (args.pseudoActivityInterval || cfg.singleRun === false) &&
+                            self.heartbeat(!cfg.singleRun ? args.pseudoActivityInterval || 5000 : args.pseudoActivityInterval);
+                    });
+                });
+            }
         });
 
+        self._process = {
+            kill: function() {
+                scope.intervals.hearbeat && clearInterval(scope.intervals.hearbeat);
+                self.browser.quit(function() {
+                    log.info('Killed ' + spec.browserName + '.');
+                    self._onProcessExit(self.error ? -1 : 0, self.error);
+                });
+            }
+        };
+
+
         self.browser.on('status', function(info) {
-            log.debug(info.cyan)
+            log.debug(info.cyan);
         });
 
         self.browser.on('command', function(eventType, command, response) {
@@ -102,40 +120,112 @@ var SeleniumWebDriver = function(baseBrowserDecorator, cfg, args, id, logger) {
         });
     };
 
-    self.kill = function(done) {
-        if (scope.promises.kill)
-            return scope.promises.kill;
-
-        var deferred = q.defer();
-        self.getSession(function(sessionID) {
-            log.debug('Browser %s requested to kill, session id is %s', spec.browserName, sessionID);
-            if (sessionID) {
-                self.driver && self.driver.quit(function(err) {
-                    log.debug('Browser %s close connection %s', spec.browserName, sessionID);
-                    if (err) {
-                        log.error('Browser %s driver failed %s %s', spec.browserName, kurl, scope.formatError(err));
-                        deferred.reject();
-                    }
-                    deferred.resolve();
-                    self._done();
-                });
-            } else {
-                self._done();
-                return deferred.reject();
-            }
-            done();
-        });
-        return (scope.promises.kill = deferred.promise);
-    };
-
-    this.killAll = function(callback) {
-        log.debug('Disconnecting all browsers')
-        if (!self.driver) {
-            return process.nextTick(callback)
+    self.kill = function() {
+        if (scope.promises.kill) {
+            return scope.promises.kill
         }
 
-        self.kill(callback);
+        var deferred = q.defer();
+
+        self.browser && self.browser.quit(function(err) {
+            log.debug('Browser %s close connection %s', spec.browserName, sessionID);
+            if (err) {
+                log.error('Browser %s driver failed %s %s', spec.browserName, kurl, scope.formatError(err));
+                deferred.reject();
+            }
+            self.state = self.FINISHED;
+            self._onProcessExit(self.error ? -1 : 0, self.error);
+            deferred.resolve();
+        });
+
+        self.state = self.BEING_KILLED
+
+        return (scope.promises.kill = deferred.promise);
+    }
+
+    self.restart = function() {
+        if (self.state === self.BEING_FORCE_KILLED) {
+            return
+        }
+
+        if (!scope.promises.kill) {
+            self.kill();
+        }
+
+        scope.promises.kill.then(function() {
+            if (self.state === self.BEING_FORCE_KILLED) {
+                self.state = self.FINISHED
+            } else {
+                scope.promises.kill = null
+                log.debug('Restarting %s', self.name)
+                self.start();
+            }
+        });
+
+        self.state = self.RESTARTING
     };
+
+    self._done = function(error) {
+        console.log('DONEE');
+        self.error = self.error || error
+        self.emit('done')
+
+        if (self.error && self.state !== self.BEING_FORCE_KILLED && self.state !== RESTARTING) {
+            emitter.emit('browser_process_failure', self)
+        }
+
+        self.state = self.FINISHED
+    }
+
+    self.forceKill = function() {
+        self.state = self.BEING_FORCE_KILLED
+        return self.kill();;
+    };
+
+    self.heartbeat = function(interval) {
+        scope.intervals.hearbeat = setTimeout(function() {
+            log.debug('Heartbeat to Selenium Hub (%s) - fetching session', spec.browserName);
+            self.browser.title(function(err, title) {
+                if (err || title !== 'Karma') {
+                    log.error('Heartbeat to %s failed %s', spec.browserName, err ? scope.formatError(err) : 'Service unavalible');
+                    clearTimeout(scope.intervals.hearbeat);
+                }
+                log.debug('Heartbeat to Selenium Hub (%s) - session active', spec.browserName);
+                self.heartbeat(interval);
+            });
+        }, interval)
+    };
+
+    self._done = function(error) {
+        scope.promises.kill = scope.promises.kill || q.defer();
+
+        self.error = self.error || error;
+
+        self.emit('done');
+
+        if (self.error && self.state !== self.BEING_FORCE_KILLED && self.state !== self.RESTARTING) {
+            emitter.emit('browser_process_failure', self)
+        }
+
+        self.state = self.FINISHED
+    }
+
+    self.toString = function() {
+        return self.name || 'Unnamed SeleniumWebdriverBrowser';
+    };
+
+    self.getSession = function(cb) {
+        if (!self.browser)
+            return cb(null);
+        cb && cb.call && cb(self.browser.sessionID);
+        return self.browser.sessionID;
+    };
+
+    self._restart = function() {};
+
+    self._onKillTimeout = function() {};
+
+    self._execCommand = function() {}
 };
 
 SeleniumWebDriver.prototype = {
@@ -145,21 +235,10 @@ SeleniumWebDriver.prototype = {
         darwin: require('wd').path,
         win32: require('wd').path
     },
-    isCaptured: function() {
-        return !!this.driver;
-    },
-    toString: function() {
-        return this.name || 'Unnamed SeleniumWebdriverBrowser';
-    },
-    getSession: function(cb) {
-        if (!this.driver)
-            return cb(null);
-        cb && cb.call && cb(this.driver.sessionID);
-        return this.driver.sessionID;
-    }
+    ENV_CMD: 'WEBDRIVER_BIN'
 };
 
-SeleniumWebDriver.$inject = ['baseBrowserDecorator', 'config', 'args', 'id', 'logger'];
+SeleniumWebDriver.$inject = ['baseBrowserDecorator', 'config', 'args', 'id', 'injector', 'emitter', 'logger'];
 
 module.exports = {
     'launcher:SeleniumWebDriver': ['type', SeleniumWebDriver]
