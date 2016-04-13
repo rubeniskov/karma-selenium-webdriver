2b4fbb3ec64077b628a4a9c687709ee65066e0e2
Rubén López Gómez <loslopez1@icloud.com>
2016-04-13 15:24:15 +0200

    remove: bump require task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,6 @@ module.exports = function(grunt) {
     grunt.initConfig({
         'npm-publish': {
             options: {
-                requires: ['bump'],
                 abortIfDirty: true,
                 tag: 'latest'
             }
