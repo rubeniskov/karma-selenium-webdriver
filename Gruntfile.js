8c55747a90e75771c3eb0d5c885add8d2b00e4c6
Rubén López Gómez <loslopez1@icloud.com>
2016-04-13 15:07:44 +0200

    add: Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        'npm-publish': {
+            options: {
+                abortIfDirty: true,
+                tag: 'latest'
+            }
+        },
+        'bump': {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'bump: release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                metadata: '',
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-npm');
+    grunt.loadNpmTasks('grunt-bump');
+    return grunt.registerTask('release', 'Bump the version and publish to NPM.', function(type) {
+        return grunt.task.run(['bump', 'npm-publish']);
+    });
+};
